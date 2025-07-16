104c1dc558b103db2827aab95cf03bce07db9c01
Danjersey007 <danjersey076@gmail.com>
2025-07-17 00:07:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ from commands.nsfw import nsfw
 from commands.ai_kyo import ai_kyo
 from commands.ban import ban     
 
-TOKEN = "LE_TOKEN_DE_TON_BOT"
+TOKEN = "7968622960:AAEo6QAtKA4YxgfjrSDpCNrBdhP8Hu6US9U"
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
